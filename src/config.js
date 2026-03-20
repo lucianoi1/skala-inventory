92685fbd641abbc0458c612d071563bb1b6d55b7
lucianoi1 <lugracial.eta18@gmail.com>
2026-03-20 16:44:48 -0600

    config.js

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// Configuración de Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyAD4A7LdNwuKPHISCeiXDdNYpbYNC6H74s",
+  authDomain: "skala-inventario.firebaseapp.com",
+  projectId: "skala-inventario"
+};
+
+// Configuración de Supabase
+export const supabaseConfig = {
+  url: "https://wcgavtxqetvdaidgcxbv.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjZ2F2dHhxZXR2ZGFpZGdjeGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNDI4MDYsImV4cCI6MjA4OTYxODgwNn0.Gz01qL2DFOuVaVCDIzf7hHFkDDLjdjZWki3sjUaDR54"
+};
